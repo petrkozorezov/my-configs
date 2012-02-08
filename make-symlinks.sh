ab7eaf14135527b88e1467a720dda19d62772ee9
Petr Kozorezov <petr.kozorezov@gmail.com>
2012-02-08 12:41:03 +0400

    mc

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd "$( dirname "$0" )"
+
 ln -s ${PWD}/emacs/main ~/.emacs
 ln -s ${PWD}/zshrc ~/.zshrc
-ln -s ${PWD}/zsh ~/.zsh
+ln -s ${PWD}/zsh/ ~/.zsh/
+mkdir -p ~/.mc/ && ln -s ${PWD}/mc/ini ~/.mc/ini
+
